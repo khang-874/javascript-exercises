c5c5fa7c60393ba427fe5b7b6bf8d14299243143
Khang Tran <tuankhangtran@cmail.carleton.ca>
2022-12-07 17:39:59 -0500

    Finish exercise from 1 to 7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...argument) {
+    let markArray = [];
+    for(let i = 0; i < array.length; ++i)
+        markArray.push(0);
+    for(let i = 0; i < argument.length; ++i){
+        for(let j = 0; j < array.length; ++j)
+            if(argument[i] === array[j])
+                markArray[j] = 1;
+    }
 
+    let result = [];
+    for(let i = 0; i < array.length; ++i)
+        if(markArray[i] == 0)
+            result.push(array[i]);
+    return result;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+function round(value, precision){
+  let multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
+const convertToCelsius = function(degree) {
+  let result = (degree - 32) * 5 / 9;
+  return round(result, 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  let result = (degree * 9 / 5 + 32);
+  return round(result, 1);
 };
 
 // Do not edit below this line
