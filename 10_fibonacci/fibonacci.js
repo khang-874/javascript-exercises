eb73e8b4bd98bc0632fd40f67ca0552370bf9470
Khang Tran <tuankhangtran@cmail.carleton.ca>
2022-12-26 16:15:32 -0500

    Finish exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if(num <= 0)
+        return "OOPS";
+    if(num <= 2 && num >= 1)
+        return 1;
+    let previous = 1;
+    let prePrevious = 1;
+    let current = 2;
+    let count = 3;
+    while(count < num){
+        current = current + previous;
+        prePrevious = previous;
+        previous = current - previous;
+        count++;
+    }
+    return current;
 };
 
 // Do not edit below this line
