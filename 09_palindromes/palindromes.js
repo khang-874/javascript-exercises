088d2a3605a032b7e6528a1d587bc5c264cd6ee7
Khang Tran <tuankhangtran@cmail.carleton.ca>
2022-12-26 16:08:50 -0500

    Finish exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let newStr = "";
+    for(let i = 0; i < str.length; ++i){
+        let code = str.charCodeAt(i);
+        if((code > 47 && code < 58) ||
+            (code > 64 && code < 91) ||
+            (code >96 && code < 123))
+                newStr += str[i];
+    }
+    newStr = newStr.toLowerCase();
+    let end = newStr.length - 1;
+    let start = 0;
+    while(start <= end){
+        if(newStr[end] != newStr[start])
+            return false;
+        start++;
+        end--
+    }
+    return true;
 };
 
 // Do not edit below this line
