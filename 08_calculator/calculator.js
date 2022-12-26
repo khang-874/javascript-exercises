dac734a806e9d8961faa9d4fd2ff26fbe9ec0e34
Khang Tran <tuankhangtran@cmail.carleton.ca>
2022-12-26 15:27:38 -0500

    Finish the exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, element) => {
+    return total += element;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, element) => {
+    return total *= element;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let result = 1;
+  for(let i = 0; i < b; ++i)
+    result *= a;
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) { 
+  if(a <= 1)
+    return 1;
+  if(a < 0)
+    return 0;
+  let result = 1;
+  for(let i = 1; i <= a; ++i)
+    result *= i;
+  return result;
 };
 
 // Do not edit below this line
